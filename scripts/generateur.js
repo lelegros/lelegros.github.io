3b21d7b5bd54f261b5f02dc40981add19db7639a
Owllyy <55492782+Owllyy@users.noreply.github.com>
2020-08-13 15:37:22 +0200

    generateur

--- scripts/generateur.js
+++ scripts/generateur.js
@@ -1,19 +1,25 @@
 var max_energie = 100;
-var cost = 0;
-var energie = max_energie - cost;
+var energie = 100;
+var generateur_stone_requirement = 10;
 
 function opengenerator()
 {
-    
+        document.getElementById("upgradetext").innerHTML = "Upgrade : " + generateur_stone_requirement + " Stone ";
 }
 
 function upgradegenerator()
-{
-    generateur.lvl++;
-    max_energie += 100;
-    energie += 100;
-    console.log(max_energie);
-    document.getElementById("fbuildingname").innerHTML = document.getElementById("generateur").name + " lvl " + generateur.lvl;
-    document.getElementById("max_energie").innerHTML = max_energie;
-    document.getElementById("energie").innerHTML = energie;
+{       
+        if (stone >= generateur_stone_requirement)
+        {
+        stone -= generateur_stone_requirement;
+        generateur_stone_requirement = Math.round(Math.exp(generateur.lvl)+10);
+        document.getElementById("upgradetext").innerHTML = "Upgrade : " + generateur_stone_requirement + " Stone ";
+        generateur.lvl++;
+        max_energie += 100;
+        energie += 100;
+        document.getElementById("fbuildingname").innerHTML = document.getElementById("generateur").name + " lvl " + generateur.lvl;
+        document.getElementById("max_energie").innerHTML = max_energie;
+        document.getElementById("energie").innerHTML = energie;
+        
+        }
 }
